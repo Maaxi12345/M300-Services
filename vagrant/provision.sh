c8d4f65557966086e83a22843e8cd1408a2c0973
Maaxi12345 <maximilianslavnic@gmail.com>
2020-06-08 09:10:53 +0200

    projectlb2.mysql

--- vagrant/provision.sh
+++
@@ -1,9 +0,0 @@
-apt-get -y update
-
-#mysql
-apt-get install -y mysql-server mysql-client
-
-mysql -u root -proot -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'root'; FLUSH privileges;"
-sudo service mysql restart
-# create client database
-mysql -u root -proot -e "CREATE DATABASE myDB;"

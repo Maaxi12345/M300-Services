3ec7137e5a94ccc7a5efd072469e98497b3f5c69
Maaxi12345 <maximilianslavnic@gmail.com>
2020-06-16 15:54:34 +0200

    projectlb2.4

--- mysql/provision.sh
+++ mysql/provision.sh
@@ -20,7 +20,6 @@ apt-get -y install mysql-server phpmyadmin
 
 
 # setup phpmyadmin
-
 apt-get -y install apache2 
 a2enmod rewrite
 
@@ -28,4 +27,24 @@ a2enmod rewrite
 link phpmyadmin to apache DirectoryRoot
 ln -fs /usr/share/phpmyadmin /var/www/phpmyadmin
 
+#neuer Virtualhost Conf mit SSL
+rm -rf /etc/apache2/sites-enabled
+cp -r /vagrant/sites-enabled /etc/apache2/
+sudo a2enmod ssl
+
+#Reverse Proxy einrichten
+sudo a2enmod proxy
+sudo a2enmod proxy_html
+sudo a2enmod proxy_http
+
 service apache2 restart
+
+#Firewall-Regeln
+sudo ufw enable
+sudo ufw allow 80/tcp
+sudo ufw allow 443/tcp
+sudo ufw allow 22/tcp
+sudo ufw allow 3306/tcp
+
+
+
